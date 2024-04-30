41ad0fb565b88be6ca8afc81b94eca34425ba378
code-kat14 <104781282+code-kat14@users.noreply.github.com>
2024-04-30 15:05:37 -0700

    updated form for html, added auth config from firebase

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-import { initializeApp } from 'firebase/app';
-const app_settings = {
-    database_url: "https://my-jiu-jitsu-app-default-rtdb.firebaseio.com/"
-}
+//const firebase = require('firebase-admin');
+import { initializeApp } from 'firebase/app'; 
+import { getDatabase } from 'firebase/database'; 
+import { getAuth, connectAuthEmulator, signInWithEmailAndPassword } from "firebase/auth";
 
-const app = initializeApp(app_settings);
+const firebaseConfig = {
+    apiKey: "AIzaSyDBeu30InEKxUvQ2seVtghxfiya9nLlSTA",
+    authDomain: "my-jiu-jitsu-app.firebaseapp.com",
+    databaseURL: "https://my-jiu-jitsu-app-default-rtdb.firebaseio.com",
+    projectId: "my-jiu-jitsu-app",
+    storageBucket: "my-jiu-jitsu-app.appspot.com",
+    messagingSenderId: "270039588756",
+    appId: "1:270039588756:web:de47e7c139d429cd7f3981"
+};
 
-console.log(app);
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+connectAuthEmulator(auth,"http://localhost:9099");
+
+/*Test to see if database is connected*/ 
+console.log("DATABASE CONNECTED");
 
 /*LOGIN PAGE*/
 const email_input = document.getElementById("email_input");
@@ -13,3 +28,13 @@ const password_input = document.getElementById("password_input");
 const login_button = document.getElementById("login");
 const register_button = document.getElementById("register");
 
+login_button.addEventListener("click", login_function());
+
+const login_function = async () => 
+{
+    const email = email_input.value;
+    const password = password_input.value;
+
+    const user_credential = signInWithEmailAndPassword(auth, email, password);
+    console.log(user_credential);
+}
